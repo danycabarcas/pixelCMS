af349be0cf081a3d680ec7e77a34599b7f0ffafa
Administrador PixelCMS <>
2026-03-19 09:33:23 -0500

    Fix: Correct Router instance using Application Core

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
-use App\Core\Router;
+/**
+ * PixelCMS - Web Routes
+ * Core Engine
+ */
+use App\Core\Application;
 use App\Controllers\AdminNewsController;
 use App\Controllers\AdminCategoryController;
 use App\Middleware\AuthMiddleware;
 
-$router = new Router();
+// Instanciar el Router a través de la aplicación
+$router = Application::$app->router;
 
 // RUTAS DE NOTICIAS
 $router->get('/admin/noticias', [AdminNewsController::class, 'index'], [AuthMiddleware::class]);
@@ -16,5 +21,3 @@ $router->post('/admin/noticias/categoria/crear', [AdminNewsController::class, 's
 $router->get('/admin/categorias', [AdminCategoryController::class, 'index'], [AuthMiddleware::class]);
 $router->post('/admin/categorias/guardar', [AdminCategoryController::class, 'store'], [AuthMiddleware::class]);
 $router->post('/admin/categorias/eliminar', [AdminCategoryController::class, 'delete'], [AuthMiddleware::class]);
-
-$router->run();
